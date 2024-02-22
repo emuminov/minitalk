227614123b4357bb318185a9b53ceae3e0d7d18a
Eldar Muminov <emuminov@z4r8p1.42lyon.fr>
2024-02-22 15:52:20 +0100

    current state

--- client.c
+++ client.c
@@ -6,24 +6,25 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 13:53:02 by emuminov          #+#    #+#             */
-/*   Updated: 2024/02/22 15:33:37 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/02/22 15:51:42 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
 
 int	main(int argc, char **argv)
 {
-	char	*pid;
+	// char	*pid;
 	char	*str;
 	int		i;
 	int		j;
 
 	if (argc != 3)
 		return (EXIT_FAILURE);
-	pid = argv[1];
+	// pid = argv[1];
 	str = argv[2];
 	i = 0;
 	while (str[i])
@@ -31,15 +32,20 @@ int	main(int argc, char **argv)
 		j = 0;
 		while (j < 8)
 		{
-			if ((str[i] & (1 << j)))
-				kill(atoi(pid), SIGUSR1);
+			if ((str[i] & (1 << (7 - j))))
+				printf("1");
+				// kill(atoi(pid), SIGUSR1);
 			else
-				kill(atoi(pid), SIGUSR2);
+				// kill(atoi(pid), SIGUSR2);
+				printf("0");
 			j++;
 		}
 		i++;
 	}
 }
+
+// 0110 0101
+
 // 0110 0101
 // 0000 0001         0000 0001
 // 0000 0010         0000 0001
