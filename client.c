31991ff8f97c973d54815c22b41f600f59edd620
Eldar Muminov <emuminov@z2r10p4.42lyon.fr>
2024-02-24 16:01:26 +0100

    refactor: refactored server code, put a lot of variables into the state struct, fixed a lot of bugs

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 13:53:02 by emuminov          #+#    #+#             */
-/*   Updated: 2024/02/24 10:40:43 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/02/24 15:09:58 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,34 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 
-void	send_bit(int bit_index, int c, int pid)
+void	send_bit(int bit_index, int bits_number, int c, int pid)
 {
-	if (c & 1 << (7 - bit_index))
+	if (c & 1 << (bits_number - 1 - bit_index))
 		kill(pid, SIGUSR1);
 	else
 		kill(pid, SIGUSR2);
 }
 
-void	send(int pid, char *str)
+void	send_size(int pid, int size, int *size_sent)
+{
+	static int	curr_bit;
+	static int	message_size;
+
+	if (!message_size && size)
+		message_size = size;
+	send_bit(curr_bit++, sizeof(int) * 8, message_size, pid);
+	if (curr_bit == sizeof(int) * 8)
+	{
+		curr_bit = 0;
+		*size_sent = 1;
+		return ;
+	}
+	usleep(1);
+}
+
+void	send_message(int pid, int *size_sent, char *str)
 {
 	static int	i;
 	static int	curr_bit;
@@ -31,21 +49,29 @@ void	send(int pid, char *str)
 
 	if (!message && str)
 		message = str;
-	send_bit(curr_bit++, message[i], pid);
-	usleep(100);
-	if (curr_bit == 8 && message[i])
+	if (!size_sent)
+		return ;
+	send_bit(curr_bit++, sizeof(char) * 8, message[i], pid);
+	if (curr_bit == sizeof(char) * 8 && message[i])
 	{
 		curr_bit = 0;
 		i++;
 	}
+	usleep(1);
 }
 
 void	handle_signal(int signal, siginfo_t *siginfo, void *context)
 {
+	static int	size_sent;
+
 	(void)context;
-	if (signal == SIGUSR1)
+	if (!size_sent && signal == SIGUSR1)
 	{
-		send(siginfo->si_pid, NULL);
+		send_size(siginfo->si_pid, 0, &size_sent);
+	}
+	else if (size_sent && signal == SIGUSR1)
+	{
+		send_message(siginfo->si_pid, &size_sent, NULL);
 	}
 	else if (signal == SIGUSR2)
 	{
@@ -68,8 +94,8 @@ int	main(int argc, char **argv)
 	sigaction(SIGUSR2, &sa, 0);
 	pid = argv[1];
 	str = argv[2];
-	send(atoi(pid), str);
-
+	send_message(atoi(pid), NULL, str);
+	send_size(atoi(pid), strlen(str), 0);
 	while (1)
 		pause();
 }
--- server.c
+++ server.c
@@ -6,49 +6,103 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 13:53:05 by emuminov          #+#    #+#             */
-/*   Updated: 2024/02/24 08:55:18 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/02/24 16:00:00 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
+#include <stdlib.h>
+
+typedef struct s_server_state
+{
+	int		size;
+	int		size_received;
+	char	*message;
+	int		message_received;
+}	t_server_state;
+
+void	receive_size(int signal, t_server_state *state)
+{
+	static int	curr_bit;
+
+	if (signal == SIGUSR1)
+	{
+		state->size |= 1 << (sizeof(int) * 8 - 1 - curr_bit);
+		curr_bit++;
+	}
+	if (signal == SIGUSR2)
+		curr_bit++;
+	if (curr_bit == sizeof(int) * 8)
+	{
+		state->size_received = 1;
+		curr_bit = 0;
+	}
+}
+
+void	handle_cleanup(int *i, int *curr_bit, char *c, t_server_state *state)
+{
+	state->message[*i] = '\0';
+	write(1, state->message, state->size);
+	write(1, "\n", 1);
+	state->message_received = 1;
+	state->size = 0;
+	state->size_received = 0;
+	*i = 0;
+	*curr_bit = 0;
+	*c = 0;
+}
+
+void	receive_message(int signal, t_server_state *state)
+{
+	static int	curr_bit;
+	static char	c;
+	static int	i;
+
+	if (signal == SIGUSR1)
+	{
+		c |= 1 << (sizeof(char) * 8 - 1 - curr_bit);
+		curr_bit++;
+	}
+	else if (signal == SIGUSR2)
+		curr_bit++;
+	if (curr_bit == sizeof(char) * 8 && c)
+	{
+		state->message[i] = c;
+		i++;
+		curr_bit = 0;
+		c = 0;
+	}
+	else if (curr_bit == sizeof(char) * 8 && !c)
+		handle_cleanup(&i, &curr_bit, &c, state);
+}
 
 void	handle_signal(int signal, siginfo_t *siginfo, void *context)
 {
-	static char	c;
-	static int	curr_bit;
+	static t_server_state	state;
 
 	(void)context;
-	if (signal == SIGUSR1)
+	if (!state.size_received)
+		receive_size(signal, &state);
+	else
 	{
-		c |= 1 << (7 - curr_bit);
-		curr_bit++;
+		if (!state.message)
+			state.message = malloc((sizeof(char) + 1) * state.size);
+		receive_message(signal, &state);
+	}
+	if ((signal == SIGUSR1 || signal == SIGUSR2) && !state.message_received)
 		kill(siginfo->si_pid, SIGUSR1);
-	}
-	if (signal == SIGUSR2)
+	else if (state.message_received)
 	{
-		curr_bit++;
-		kill(siginfo->si_pid, SIGUSR1);
-	}
-	if (curr_bit == 8 && c)
-	{
-		write(1, &c, 1);
-		curr_bit = 0;
-		c = 0;
-	}
-	else if (curr_bit == 8 && !c)
-	{
-		write(1, "\n", 1);
-		curr_bit = 0;
-		c = 0;
+		state.message_received = 0;
 		kill(siginfo->si_pid, SIGUSR2);
 	}
 }
 
 int	main(void)
 {
-	int 				pid;
+	int					pid;
 	struct sigaction	sa;
 
 	pid = getpid();
@@ -58,5 +112,5 @@ int	main(void)
 	sigaction(SIGUSR1, &sa, 0);
 	sigaction(SIGUSR2, &sa, 0);
 	while (1)
-	{}
+		usleep(1);
 }
