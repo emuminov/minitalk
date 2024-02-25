95c10381fe4ba3e2784be732ab67d6b55f63f91a
pdlmn <jseldardw@gmail.com>
2024-02-25 19:30:35 +0100

    feat: converted sizeof's to actual numbers

--- minitalk.h
+++ minitalk.h
@@ -6,13 +6,13 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 17:06:06 by emuminov          #+#    #+#             */
-/*   Updated: 2024/02/24 17:10:17 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/02/25 19:17:52 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 # include "libft/libft.h"
-# define CHAR_BITS (sizeof(char) * 8)
-# define INT_BITS (sizeof(int) * 8)
+# define CHAR_BITS 8
+# define INT_BITS 32
 #endif
